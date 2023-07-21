73c4b6d2deffe9610e82c03413aeaeea677ba2f7
zsmalla <malla7454@gmail.com>
2023-07-21 16:30:17 +0900

    feat : connect Post URL into finish button

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -11,47 +11,47 @@
         </span>
       </div>
     </div>
-    <Vueform :force-labels="true">
+    <Vueform :force-labels="true" :v-model="this.formData" endpoint="http://127.0.0.1:8000/api/v1/coffee-beans/recommended/" method="post">
       <template #empty>
         <FormSteps>
           <FormStep
             name="Step1"
             label="Step1"
-            :elements="['first_input', 'second_input']"
+            :elements="['gender', 'ageRange']"
           />
           <FormStep
             name="Step2"
             label="Step2"
-            :elements="['third_input']"
+            :elements="['favorite_scent']"
           />
           <FormStep
             name="Step3"
             label="Step3"
-            :elements="['fourth_input', 'fifth_input', 'sixth_input', 'seventh_input']"
+            :elements="['acidity', 'sweetness', 'body_feel', 'roasting_characteristics']"
           />
         </FormSteps>
 
         <FormElements>
-          <RadiogroupElement name="first_input" placeholder = "first_input" view="blocks"
-          :items="[ { value:'남자', label:'남자' }, { value:'여자', label:'여자' }  ]"
+          <RadiogroupElement name="gender" placeholder = "gender" view="blocks"
+          :items="[ { value:'M', label:'남자' }, { value:'F', label:'여자' }  ]"
           label="당신의 성별을 알려주세요"/>
 
-          <RadiogroupElement name="second_input" placeholder = "second_input" view="blocks"
+          <RadiogroupElement name="age" placeholder = "age" view="blocks"
           :items="[ { value:'10대', label:'10대' }, { value:'20대', label:'20대' }, { value:'30대', label:'30대' }, { value:'40대', label:'40대' }, { value:'50대', label:'50대' }, { value:'60대 이상', label:'60대 이상' }]"
           label="당신의 연령대를 알려주세요"/>
-          <RadiogroupElement name="third_input" placeholder = "third_input" view="blocks"
-          :items="[ { value:'초콜릿향', label:'초콜릿향' }, { value:'고소한 견과류향', label:'고소한 견과류향' }, { value:'상큼한 과일향', label:'상큼한 과일향' }, { value:'은은한 꽃향', label:'은은한 꽃향' }]"
+          <RadiogroupElement name="favorite_scent" placeholder = "favorite_scent" view="blocks"
+          :items="[ { value:'chocolate', label:'초콜릿향' }, { value:'nutty', label:'고소한 견과류향' }, { value:'fruity', label:'상큼한 과일향' }, { value:'floral', label:'은은한 꽃향' }]"
           label="커피를 드실 때 선호하는 향을 알려주세요"/>
-          <SliderElement name="fourth_input" placeholder = "fourth_input" :defalut="5" :max="10" :min="0" :step="1"
+          <SliderElement name="acidity" placeholder = "acidity" :defalut="5" :max="10" :min="0" :step="1"
           label="커피를 드실 때 선호하는 산미의 정도를 알려주세요"
           info="산미는 원두가 가지고 있는 신 맛의 정도를 나타냅니다." show-tooltip="focus" tooltip-position="bottom"/>
-          <SliderElement name="fifth_input" placeholder = "fifth_input" :defalut="5" :max="10" :min="0" :step="1"
+          <SliderElement name="sweetness" placeholder = "sweetness" :defalut="5" :max="10" :min="0" :step="1"
           label="커피를 드실 때 선호하는 단 맛의 정도를 알려주세요"
           info="단 맛은 원두가 가지고 있는 단 맛의 정도를 나타냅니다." show-tooltip="focus" tooltip-position="bottom"/>
-          <SliderElement name="sixth_input" placeholder = "sixth_input" :defalut="5" :max="10" :min="0" :step="1"
+          <SliderElement name="body_feel" placeholder = "body_feel" :defalut="5" :max="10" :min="0" :step="1"
           label="커피를 드실 때 선호하는 바디감의 정도를 알려주세요"
           info="바디감은 커피가 입 안으로 들어왔을 때 주는 무게감을 나타냅니다." show-tooltip="focus" tooltip-position="bottom"/>
-          <SliderElement name="seventh_input" placeholder = "seventh_input" :defalut="5" :max="10" :min="0" :step="1"
+          <SliderElement name="roasting_characteristics" placeholder = "roasting_characteristics" :defalut="5" :max="10" :min="0" :step="1"
           label="커피를 드실 때 선호하는 원두 로스팅의 정도를 알려주세요"
           info="로스팅은 원두를 가열하여 볶은 정도를 의미합니다." show-tooltip="focus" tooltip-position="bottom"/>
         </FormElements>
@@ -59,7 +59,7 @@
       </template>
     </Vueform>
   </div>
-  <TestResult v-if="test_done"></TestResult>
+  <TestResult v-if="this.test_done"></TestResult>
 </template>
 
 <script>
@@ -67,15 +67,14 @@ import TestResult from "./TestResult.vue";
 export default {
   data() {
     return {
-      gender: "",
-      ageRange: "",
-      flavor: "",
-      acid: 0,
-      sweet: 0,
-      bodyRate: 0,
-      roast: 0,
-      test_done: false
+      test_done: false,
+      formData : {},
     };
+  },
+  methods:{
+    handleSubmit(){
+      console.log(this.formData)
+    },
   },
   components: {
     TestResult,
